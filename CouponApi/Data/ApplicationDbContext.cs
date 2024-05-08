c5c0cd2435b2bebf6400a8a6003be7ada28c8caa
kourosh <kourosh23@hotmail.com>
2024-05-08 12:46:31 +0200

    coupon seed + apply migraton

--- CouponApi/Data/ApplicationDbContext.cs
+++ CouponApi/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace CouponApi.Data
         }
 
         public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon { Id = 1, Code="10off", DiscountAmount=10, MinAmount=20 });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon { Id = 2, Code="20off", DiscountAmount=20, MinAmount=40 });
+        }
     }
 }
--- CouponApi/Program.cs
+++ CouponApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
     throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -30,5 +30,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    if (db.Database.GetPendingMigrations().Any()) { db.Database.Migrate(); }
+}
 
 app.Run();
