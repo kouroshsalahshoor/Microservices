7d137252dae23627768b8a11553245345fc94c45
kourosh <kourosh23@hotmail.com>
2024-05-08 13:10:07 +0200

    couponapi get endpoints

---
+++ CouponApi/Controllers/CouponApiController.cs
@@ -0,0 +1,59 @@
+﻿using CouponApi.Data;
+using CouponApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace CouponApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ResponseDto _response;
+
+        public CouponApiController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new();
+        }
+
+        [HttpGet]
+        public async Task<ResponseDto> Get()
+        {
+            try
+            {
+                var models = await _db.Coupons.ToListAsync();
+                _response.IsSuccessful = true;
+                _response.Result = models;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Errors.Add(ex.Message);
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                var model = await _db.Coupons.FirstOrDefaultAsync(x=>x.Id == id);
+                _response.IsSuccessful = true;
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccessful = false;
+                _response.Errors.Add(ex.Message);
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Shared/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public class ResponseDto
+    {
+        public bool IsSuccessful { get; set; }
+        public object? Result { get; set; }
+        public List<string> Errors { get; set; } = new();
+    }
+}
