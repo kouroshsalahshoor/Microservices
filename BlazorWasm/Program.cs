426c8e9187d3f0f76fa56d5bd5fb9c5293654d8b
kourosh <kourosh23@hotmail.com>
2024-05-14 10:34:17 +0200

    appsettings

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,11 +1,22 @@
+using BlazorWam.Services;
 using BlazorWasm;
+using BlazorWasm.Services.IServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Shared.Front;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+ApplicationConstants.CouponApi = builder.Configuration["ServiceUrls:CouponApi"];
+
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 await builder.Build().RunAsync();
