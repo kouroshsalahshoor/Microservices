ee0293ffe378040e0ddf42903338f5c4a16901b0
kourosh <kourosh23@hotmail.com>
2024-05-14 14:01:10 +0200

    toastr

---
+++ BlazorWasm/Utilities/JsRuntimeExtentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWasm.Utilities
+{
+    public static class JsRuntimeExtentions
+    {
+        //For Success Message of an opration
+        public static ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            return jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        //for error message of an opration
+        public static ValueTask ToastrError(this IJSRuntime jsSRuntime, string message)
+        {
+            return jsSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+    }
+}
