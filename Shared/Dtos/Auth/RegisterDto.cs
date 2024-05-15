475dd56b2b911411049488719d6e1ae73d773538
kourosh <kourosh23@hotmail.com>
2024-05-15 09:24:12 +0200

    auth dtos

---
+++ Shared/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Dtos.Auth
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
