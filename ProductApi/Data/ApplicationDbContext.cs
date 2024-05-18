29540a2fefbf2bc8702abef995856060b736cd62
kourosh <kourosh23@hotmail.com>
2024-05-18 08:59:39 +0200

    productapi

---
+++ ProductApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                modelBuilder.Entity<Product>().HasData(new Product
+                {
+                    Id = 1,
+                    Name = "Product Name " + i.ToString(),
+                    Price = i * 100,
+                    Description = "Description for Product " + i.ToString(),
+                    ImageUrl = "https://placeholder.co/603x403",
+                    Category = i % 2 == 0 ? "Category 2" : "Category 1",
+                });
+            }
+        }
+    }
+}
---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
