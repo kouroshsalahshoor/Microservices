69979ec01c7525958e87c0f90627063b9162250f
kourosh <kourosh23@hotmail.com>
2024-06-01 11:01:53 +0200

    email cart

---
+++ EmailApi/MessageConsumers/RabbitMQCartConsumer.cs
@@ -0,0 +1,59 @@
+﻿
+using EmailApi.Services;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared.Dtos.Cart;
+using System.Text;
+
+namespace EmailApi.MessageConsumers
+{
+    public class RabbitMQCartConsumer : BackgroundService
+    {
+        private readonly EmailLoggerService _emailLoggerService;
+        private IModel _channel;
+        private string _queueName;
+
+        public RabbitMQCartConsumer(IConfiguration configuration, EmailLoggerService emailLoggerService)
+        {
+            _emailLoggerService = emailLoggerService;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            var connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+
+            _queueName = configuration.GetValue<string>("TopicAndQueueNames:CartQueue")!;
+            _channel.QueueDeclare(_queueName, durable: false, exclusive: false, autoDelete: false, null);
+
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += async (ch, ev) =>
+            {
+                var content = Encoding.UTF8.GetString(ev.Body.ToArray());
+                var cart = JsonConvert.DeserializeObject<CartDto>(content);
+                await handleMessage(cart!);
+
+                _channel.BasicAck(ev.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(_queueName, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task handleMessage(CartDto dto)
+        {
+            await _emailLoggerService.Cart(dto);
+        }
+    }
+}
--- EmailApi/Services/EmailLoggerService.cs
+++ EmailApi/Services/EmailLoggerService.cs
@@ -2,6 +2,8 @@
 using EmailApi.Models;
 using EmailApi.Services.IServices;
 using Microsoft.EntityFrameworkCore;
+using Shared.Dtos.Cart;
+using System.Text;
 
 namespace EmailApi.Services
 {
@@ -13,13 +15,31 @@ namespace EmailApi.Services
         {
             _dbOptions = dbOptions;
         }
+
+        public async Task Cart(CartDto dto)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("<br/>Cart Email:");
+            stringBuilder.AppendLine("<br/>Total: " + dto.CartHeader.Total);
+            stringBuilder.AppendLine("<br/>");
+            stringBuilder.AppendLine("<ul>");
+            foreach (var item in dto.CartDetails!)
+            {
+            stringBuilder.AppendLine("<li>");
+            stringBuilder.AppendLine(item.Product!.Name + " x " + item.Count);
+            stringBuilder.AppendLine("</li>");
+            }
+            stringBuilder.AppendLine("</ul>");
+
+            await save(stringBuilder.ToString(), dto.CartHeader.Email);
+        }
         public async Task RegisterUser(string email)
         {
             string message = "User Registeration Successful. <br/> Email : " + email;
-            await saveAndEmail(message, "admin@x.x");
+            await save(message, "admin@x.x");
         }
 
-        private async Task<bool> saveAndEmail(string message, string email)
+        private async Task<bool> save(string message, string email)
         {
             try
             {
@@ -36,8 +56,8 @@ namespace EmailApi.Services
             }
             catch (Exception ex)
             {
-                return false;
             }
+            return false;
         }
     }
 }
